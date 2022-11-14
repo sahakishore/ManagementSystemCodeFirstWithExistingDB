5583d969a22256fc43a7cff83b6ba9e2b8787890
Kishore Saha <sahakishore4@gmail.com>
2022-11-14 13:02:33 +0530

    added all funtions in managementController

---
+++ ManagementSystemCodeFirstWithExistingDB/Controllers/EmployeeManagementController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+
+namespace ManagementSystemCodeFirstWithExistingDB.Controllers
+{
+    public class EmployeeManagementController 
+    {
+        private readonly ManagementSystemContext _context;
+        public EmployeeManagementController()
+        {
+            _context = new ManagementSystemContext();
+        }
+
+
+        //POST:  Create employee passing name, email, phone and roleid
+        /*
+         Create new employee
+        */
+        public void CreateNewEmployee(string name,int roleId,string email,int phone)
+        {
+          
+
+            var employeeContext = _context.Employees;
+
+            //var roleContext = context.Roles.ToList();
+
+            //var roleContextNew = context.Roles.Single(r => r.Id == 1);
+
+            //adding EmployeeDetails context
+            var empDetailsContext = _context.EmployeeDetails;
+
+            var newEmpDetails = new EmployeeDetail
+            {
+                Email = email,
+                Phone = phone,
+
+            };
+
+
+            var newEmp = new Employee
+            {
+                Name = name,
+
+                //ForeignKey approach
+                RoleId = roleId,
+
+                //employee details
+                EmployeeDetail = newEmpDetails,
+
+
+            };
+
+            employeeContext.Add(newEmp);
+            _context.SaveChanges();
+
+            Console.WriteLine("New Employee Creation Successful");
+
+        }
+
+
+        //PUT:  Update existing employee
+        /*
+         Updating Employee by EmployeeId
+        */
+        public void UpdateExistingEmployee(int empId, string name, int roleId, string email, int phone)
+        {
+
+            var employeeContext = _context.Employees;
+            //Updating EmployeeDetails context
+            var empDetailsContext = _context.EmployeeDetails;
+
+            var existingEmpDetails = empDetailsContext.Find(empId);
+            existingEmpDetails.Email = email;
+            existingEmpDetails.Phone = phone;
+
+            //Find Employee by Id
+            var existedEmployee = employeeContext.Find(empId);
+
+            existedEmployee.Name = name;
+            existedEmployee.RoleId = roleId;
+            existedEmployee.EmployeeDetail = existingEmpDetails;
+
+            _context.SaveChanges();
+
+            Console.WriteLine("Updated Employee with ID:" + existedEmployee.Name);
+        }
+
+
+        //Get:  Get the list of all employees
+
+        /*Get list of all employees*/
+        public List<Employee> GetAllEmployee()
+        {
+            var employee = _context.Employees.ToList();
+
+            return employee;
+        }
+
+        //Get:  Get details of single employees on the basis if employid
+
+        /*get details of single employee by employeId */
+        public string GetEmployeeById(int empId, string empName)
+        {
+
+            var empContext = _context.Employees;
+            var empDetails = _context.EmployeeDetails;
+
+            /*     var employee =
+                     from c in context.Employees
+                     where c.EmployeeId == empId
+                     select c;
+
+                 */
+
+            var employee =
+                empContext
+               .Include(c => c.EmployeeDetail)
+               .SingleOrDefault(e => e.EmployeeId == empId);
+
+
+            //foreach (var e in employee)
+            //{
+            //    //Console.WriteLine(e.Name);
+            //    //return e.Name;
+            //    empName = e.Name;
+            //}
+
+            Console.WriteLine("VVV Employee details Name: {0} ,  Email: {1}", employee.Name, employee.EmployeeDetail.Email);
+
+            empName = employee.Name;
+            return empName;
+
+        }
+
+
+
+    }
+}
--- ManagementSystemCodeFirstWithExistingDB/Program.cs
+++ ManagementSystemCodeFirstWithExistingDB/Program.cs
@@ -1,236 +1,76 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Script.Serialization;
-//using System.EntityF
-//using System.Web.Script.Serialization;
+using ManagementSystemCodeFirstWithExistingDB.Controllers;
 
 namespace ManagementSystemCodeFirstWithExistingDB
 {
     class Program
     {
-        
-       
-      
         static void Main(string[] args)
         {
-            //create DbContext
-            //var context = new ManagementSystemContext();
+            //taking input from consumer
+
+            var empDetailsPost = new
+            {
+                name = "New Empolyee add Test 9",
+                roleId = 1,
+                email = "test8@gmail.com",
+                phone = 923456789,
+
+            };
+
+            var empDetailsPut = new
+            {
+                empId = 12,
+                name = "Edited Empolyee add Test 12",
+                roleId = 1,
+                email = "changed12@gmail.com",
+                phone = 123456789,
+
+            };
+
+            var empDetailsGet = new
+            {
+                empId = 8,
+                empName = "",
+            };
+
+
+            var employeeContext = new EmployeeManagementController();
+           
+
+
+            //creating new Employee
+            employeeContext.CreateNewEmployee(empDetailsPost.name, empDetailsPost.roleId, empDetailsPost.email, empDetailsPost.phone);
+
+            /*get details of single employee by employeId */
+            //calling GetEmployeeById method
+           
+
+            string empDetailsById = employeeContext.GetEmployeeById(empDetailsGet.empId, empDetailsGet.empName);
+            Console.WriteLine("Employee details:" + empDetailsById);
+
+            
+
+
+            //Update Existing Employee
+            employeeContext.UpdateExistingEmployee(empDetailsPut.empId, empDetailsPut.name, empDetailsPut.roleId, empDetailsPut.email, empDetailsPut.phone);
 
 
 
             /*Get list of all Employee */
-
-
-
-            /*get details of single employee by employeId */
-
-            /*   var employee =
-                   from c in context.Employees
-                   where c.EmployeeId == 1
-                   select c;
-
-               foreach(var e in employee)
-               {
-                   Console.WriteLine(e.Name);
-               }*/
-
-
-            //creating new Employee
-            //CreateNewEmployee();
-
-
-            //Update Existing Employee
-            UpdateExistingEmployee();
-
-            //calling GetEmployeeById method
-            string empDetails = GetEmployeeById( 8, "");
-            Console.WriteLine("Employee details:" + empDetails);
-
-
             //calling GetAllEmployee method
-            var allEmp = GetAllEmployee();
+
+            var allEmployee = employeeContext.GetAllEmployee();
 
 
             Console.WriteLine("All Employees:");
-            foreach (var e in allEmp)
+            foreach (var e in allEmployee)
             {
                 Console.WriteLine(e.Name);
             }
 
 
-            
-
-
-
         }
-
-
-        /*get details of single employee by employeId */
-        public static string GetEmployeeById( int empId, string empName)
-        {
-          
-            var context = new ManagementSystemContext();
-
-            /*var employee =
-                from c in context.Employees
-                where c.EmployeeId == empId
-                select c;*/
-
-            /* var employee =
-                 context.Employees
-                 .SingleOrDefault(a => a.EmployeeId == empId);
-            */
-
-            var employee =
-               context.Employees
-               .Include(c => c.EmployeeDetail)
-               .SingleOrDefault(e => e.EmployeeId == empId);
-
-
-            //foreach (var e in employee)
-            //{
-            //    //Console.WriteLine(e.Name);
-            //    //return e.Name;
-            //    empName = e.Name;
-            //}
-
-            Console.WriteLine("VVV Employee details Name: {0} ,  Email: {1}" , employee.Name, employee.EmployeeDetail.Email);
-
-            empName = employee.Name;
-            return empName;
-
-           
-
-        }
-
-
-
-        /*Get list of all employees*/
-        public static List<Employee> GetAllEmployee()
-        {
-
-            var context = new ManagementSystemContext();
-
-            var employee = context.Employees.ToList();
-
-
-
-
-            //foreach (var e in employee)
-            //{
-            //    Console.WriteLine(e.Name);
-            //    //return e.Name;
-            //    //empName = e.Name;
-            //}
-
-
-            //return empName;
-            return employee;
-
-
-        }
-
-
-        /*
-         Create new employee
-         */
-
-        public static void CreateNewEmployee()
-        {
-
-            var context = new ManagementSystemContext();
-
-            var employeeContext = context.Employees;
-
-
-
-            //var roleContext = context.Roles.ToList();
-
-            //here id comes as input , assign employee with a single role
-            //var roleContextNew = context.Roles.Single(r => r.Id == 1);
-
-            //adding EmployeeDetails context
-            var empDetailsContext = context.EmployeeDetails;
-
-            var newEmpDetails = new EmployeeDetail
-            {
-                Email = "test3@gmail.com",
-                Phone = 323456789,
-
-            };
-            //empDetailsContext.Add
-
-
-
-
-            var newEmp = new Employee
-            {
-                Name = "New Empolyee add Test 3",
-
-                //ForeignKey approach
-                RoleId = 1,
-
-               //Role = roleContextNew,
-
-                //employee details
-                EmployeeDetail = newEmpDetails,
-
-
-            };
-
-            employeeContext.Add(newEmp);
-            context.SaveChanges();
-            //await _context.SaveChangesAsync();
-
-            Console.WriteLine("New Employee Creation Successful");
-
-        }
-
-
-        /*
-         Updating Employee by EmployeeId
-         */
-
-        public static void UpdateExistingEmployee()
-        {
-            var context = new ManagementSystemContext();
-
-            var employeeContext = context.Employees;
-            //Updating EmployeeDetails context
-            var empDetailsContext = context.EmployeeDetails;
-
-            /* var editEmpDetails = new EmployeeDetail
-             {
-                 Email = "changed3@gmail.com",
-                 Phone = 332356789,
-
-             };*/
-            var existingEmpDetails = empDetailsContext.Find(8);
-            existingEmpDetails.Email = "changed3@gmail.com";
-            existingEmpDetails.Phone = 332356789;
-
-            //Find Employee by Id
-            var existedEmployee = employeeContext.Find(8);
-
-            existedEmployee.Name = "Edited Empolyee add Test 3";
-            existedEmployee.RoleId = 2;
-            existedEmployee.EmployeeDetail = existingEmpDetails;
-            //employeeContext.AddOrUpdate(existedEmployee);
-
-            context.SaveChanges();
-            
-
-
-            Console.WriteLine("Updated Employee with ID:" + existedEmployee.Name);
-        }
-
 
     }
 }
